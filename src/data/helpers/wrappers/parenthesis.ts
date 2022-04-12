947079f1ef5bcbd9135d15331c8221aa998c1bf2
Jimmy Moon <ragingwind@gmail.com>
2022-04-13 08:25:03 +0900

    feat: prevent double parenthesis

--- src/data/helpers/wrappers/parenthesis.ts
+++ src/data/helpers/wrappers/parenthesis.ts
@@ -3,13 +3,14 @@ import { Wrapper } from './wrapper';
 export class ParenthesisWrapper {
   protected _open = '\\left(';
   protected _close = '\\right)';
+  protected _closed = /^(\\left\(\\right.).*(\\left.\\right\))$/i;
 
   wrap(str: string): string {
     return new Wrapper(this._open, this._close).wrap(str);
   }
 
   wrapIfMoreThanOneChar(str: string): string {
-    if (str.length <= 1) return str;
+    if (str.length <= 1 || this._closed.test(str) == true) return str;
     return this.wrap(str);
   }
 }
